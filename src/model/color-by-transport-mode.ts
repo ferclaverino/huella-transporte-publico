702708b8f115eae474538d485c2eb475cb4357b5
Fernando Claverino <fernando.claverino@gmail.com>
2024-08-12 19:46:00 -0300

    define colors

--- src/model/color-by-transport-mode.ts
+++ src/model/color-by-transport-mode.ts
@@ -2,10 +2,10 @@ import { TransportMode } from "./transport-mode";
 
 // emission (in CO2 gr) = factor * distance (in km)
 export const colorByTransport: Record<TransportMode, string> = {
-  CAR: "#73b9ff",
-  BUS: "#5cb85c",
-  SUBWAY: "blue",
-  TRAIN: "yellow",
-  BIKE: "orange",
-  WALK: "green",
+  CAR: "#e11e26",
+  BUS: "#193662",
+  SUBWAY: "#f1882c",
+  TRAIN: "#f4ba1b",
+  BIKE: "#2c6435",
+  WALK: "#a0c539",
 };
