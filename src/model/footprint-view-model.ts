7dbdd2b7016009d1933d313ca0cd4120fe456fc8
Fernando Claverino <fernando.claverino@gmail.com>
2024-08-13 11:48:46 -0300

    format decimals

--- src/model/footprint-view-model.ts
+++ src/model/footprint-view-model.ts
@@ -1,13 +1,24 @@
 import { Footprint } from "./footprint";
 
+const locale = "es-AR";
+
 export class FootprintViewModel {
   readonly distanceInKm: string;
   readonly emissionsInGr: string;
   readonly durationInMin: string;
 
   constructor(footprint: Footprint) {
-    this.distanceInKm = (footprint.distance / 1000).toFixed(1);
-    this.emissionsInGr = footprint.emissions.toFixed(2);
-    this.durationInMin = (footprint.duration / 60).toFixed();
+    this.distanceInKm = (footprint.distance / 1000).toLocaleString(locale, {
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 1,
+    });
+    this.emissionsInGr = footprint.emissions.toLocaleString(locale, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+    this.durationInMin = (footprint.duration / 60).toLocaleString(locale, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
   }
 }
