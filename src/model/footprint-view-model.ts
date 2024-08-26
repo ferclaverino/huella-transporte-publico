b8141d32afad4bf57889d8dc51c821ecaf74a09e
Fernando Claverino <fernando.claverino@gmail.com>
2024-08-26 10:53:13 -0300

    test for decimals

--- src/model/footprint-view-model.ts
+++ src/model/footprint-view-model.ts
@@ -4,21 +4,21 @@ const locale = "es-AR";
 
 export class FootprintViewModel {
   readonly distanceInKm: string;
-  readonly emissionsInGr: string;
   readonly durationInMin: string;
+  readonly emissionsInGr: string;
 
   constructor(footprint: Footprint) {
     this.distanceInKm = (footprint.distance / 1000).toLocaleString(locale, {
       minimumFractionDigits: 1,
       maximumFractionDigits: 1,
     });
-    this.emissionsInGr = footprint.emissions.toLocaleString(locale, {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
     this.durationInMin = (footprint.duration / 60).toLocaleString(locale, {
       minimumFractionDigits: 0,
       maximumFractionDigits: 0,
+    });
+    this.emissionsInGr = footprint.emissions.toLocaleString(locale, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
     });
   }
 }
--- test/footprint-view-model.test.ts
+++ test/footprint-view-model.test.ts
@@ -12,4 +12,32 @@ describe("FootprintViewModel", () => {
       emissionsInGr: "0,00",
     });
   });
+
+  test("given footprint with integer numbers, then get view model", () => {
+    const footprintViewModel = new FootprintViewModel({
+      distance: 1000,
+      duration: 60,
+      emissions: 3,
+      transportModes: [],
+    });
+    expect(footprintViewModel).toEqual({
+      distanceInKm: "1,0",
+      durationInMin: "1",
+      emissionsInGr: "3,00",
+    });
+  });
+
+  test("given footprint with decimal numbers, then get view model", () => {
+    const footprintViewModel = new FootprintViewModel({
+      distance: 1500,
+      duration: 90,
+      emissions: 3.45,
+      transportModes: [],
+    });
+    expect(footprintViewModel).toEqual({
+      distanceInKm: "1,5",
+      durationInMin: "2",
+      emissionsInGr: "3,45",
+    });
+  });
 });
